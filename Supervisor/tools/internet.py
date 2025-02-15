192ab4ba6585b1ea5e86875caa1643be2f7e3741
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2025-02-15 14:46:46 +0530

    Add files via upload

---
+++ Supervisor/tools/internet.py
@@ -0,0 +1,26 @@
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.tools import tool
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
+from langchain_community.tools.tavily_search import TavilySearchResults
+from pydantic import BaseModel, Field
+
+
+class InternetTool(BaseModel):
+    """Input for the Tavily Tool"""
+    query: str = Field(description="search query to look up")
+
+@tool
+def internet_tool(query:str):
+    """Input for the Tavily Tool"""
+    wrapper = TavilySearchAPIWrapper()
+    tavily_tool = TavilySearchResults(
+        name="Internet-Tool",
+        api_wrapper=wrapper,
+        max_results=5,
+        args_schema=InternetTool,
+    )
+    return tavily_tool.run({"query": query})
---
+++ Supervisor/tools/temperature.py
@@ -0,0 +1,19 @@
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.utilities import OpenWeatherMapAPIWrapper
+from langchain_core.tools import tool
+from pydantic import BaseModel, Field
+
+class TemperatureInput(BaseModel):
+    """Fetch current temperature for give place"""
+    query: str = Field(description="Fetch current temperature for given place")
+
+@tool
+def temperature_tool(query:str):
+    """Fetch current temperature for give place"""
+    validated_input = TemperatureInput(query=query)
+    weather = OpenWeatherMapAPIWrapper()
+    return weather.run(validated_input.query)
+
