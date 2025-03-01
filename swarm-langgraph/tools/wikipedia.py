ba79bc2f739119327a80a60dd3fad4975ae42959
Anantha Narayanan <125557793+ananthanarayanan431@users.noreply.github.com>
2025-03-02 00:29:10 +0530

    Add files via upload

---
+++ swarm-langgraph/tools/wikipedia.py
@@ -0,0 +1,24 @@
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.tools import tool
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from pydantic import BaseModel, Field
+
+class WikiInputs(BaseModel):
+    """Inputs to the Wikipedia tool"""
+    query:str = Field(description="Query to look up in wikipedia, should 3 or less words")
+
+
+def wikipedia_tool(query:str):
+    """Inputs to the Wikipedia tool"""
+    wrapper = WikipediaAPIWrapper(top_k_results=2,doc_content_chars_max=2000)
+    tool = WikipediaQueryRun(
+        name="Wiki-Input",
+        api_wrapper=wrapper,
+        args_schema=WikiInputs,
+        return_direct=True,
+    )
+    return tool.run({"query": query})
